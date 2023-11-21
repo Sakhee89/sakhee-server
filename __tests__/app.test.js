dc7ea5f7d42de014a21f3b8d309f7f03e18a2741
Sakhee89 <kung_fu_fighting_monkey@hotmail.com>
2023-11-21 16:08:04 +0000

    made test a little clearer

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,210 +1,219 @@
 const request = require("supertest");
-const db = require("../db/connection.js")
-const seed = require("../db/seeds/seed.js")
-const testData = require("../db/data/test-data/index.js")
-const app = require("../app.js")
-const endPoints = require("../endpoints")
+const db = require("../db/connection.js");
+const seed = require("../db/seeds/seed.js");
+const testData = require("../db/data/test-data/index.js");
+const app = require("../app.js");
+const endPoints = require("../endpoints");
 
 afterAll(() => {
-    return db.end();
-})
+  return db.end();
+});
 
 beforeEach(() => {
-    return seed(testData)
-})
+  return seed(testData);
+});
 
 describe("/api", () => {
-    test("Get: 200 sends an object of all available endpoints", () => {
-        return request(app)
-        .get("/api")
-        .expect(200)
-        .then((response) => {
-            expect(response.body.apiEndPoints).toEqual(endPoints)
-        })
-    })
-})
+  test("Get: 200 sends an object of all available endpoints", () => {
+    return request(app)
+      .get("/api")
+      .expect(200)
+      .then((response) => {
+        expect(response.body.apiEndPoints).toEqual(endPoints);
+      });
+  });
+});
 
 describe("/api/topics", () => {
-    test("Get: 200 sends an array of topics to the client", () => {
-        return request(app)
-        .get("/api/topics")
-        .expect(200)
-        .then((response) => {
-            expect(response.body.topics.length).toBe(3);
-            response.body.topics.forEach((topic) => {
-                expect(typeof topic.slug).toBe('string')
-                expect(typeof topic.description).toBe('string')
-            })
-        })
-    })
-})
+  test("Get: 200 sends an array of topics to the client", () => {
+    return request(app)
+      .get("/api/topics")
+      .expect(200)
+      .then((response) => {
+        expect(response.body.topics.length).toBe(3);
+        response.body.topics.forEach((topic) => {
+          expect(typeof topic.slug).toBe("string");
+          expect(typeof topic.description).toBe("string");
+        });
+      });
+  });
+});
 
 describe("/api/articles", () => {
-    test("Get: 200 sends an array of topics to the client excluding a body property in the object", () => {
-        return request(app)
-        .get("/api/articles")
-        .expect(200)
-        .then((response) => {
-            expect(response.body.articles.length).toBe(13);
-            response.body.articles.forEach((article) => {
-                expect(typeof article.author).toBe('string')
-                expect(typeof article.title).toBe('string')
-                expect(typeof article.article_id).toBe('number')
-                expect(typeof article.topic).toBe('string')
-                expect(typeof article.created_at).toBe('string')
-                expect(typeof article.votes).toBe('number')
-                expect(typeof article.article_img_url).toBe('string')
-                expect(typeof article.comment_count).toBe('string')
-                expect(typeof article.body).toBe('undefined')
-            })
-        })
-    })
+  test("Get: 200 sends an array of topics to the client excluding a body property in the object", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then((response) => {
+        expect(response.body.articles.length).toBe(13);
+        response.body.articles.forEach((article) => {
+          expect(typeof article.author).toBe("string");
+          expect(typeof article.title).toBe("string");
+          expect(typeof article.article_id).toBe("number");
+          expect(typeof article.topic).toBe("string");
+          expect(typeof article.created_at).toBe("string");
+          expect(typeof article.votes).toBe("number");
+          expect(typeof article.article_img_url).toBe("string");
+          expect(typeof article.comment_count).toBe("string");
+          expect(typeof article.body).toBe("undefined");
+        });
+      });
+  });
 
-    test("Get: 200 sends an array of topics to the client and the object matches the shape", () => {
-        return request(app)
-        .get("/api/articles")
-        .expect(200)
-        .then((response) => {
-            const article = {
-            "article_id": 3,
-            "article_img_url": "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
-            "author": "icellusedkars", "comment_count": "2", "created_at": "2020-11-03T09:12:00.000Z",
-            "title": "Eight pug gifs that remind me of mitch",
-            "topic": "mitch",
-            "votes": 0}
-            expect(response.body.articles[0]).toMatchObject(article);
-        })
-    })
+  test("Get: 200 sends an array of topics to the client and the object matches the shape", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then((response) => {
+        const article = {
+          article_id: 3,
+          article_img_url:
+            "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+          author: "icellusedkars",
+          comment_count: "2",
+          created_at: "2020-11-03T09:12:00.000Z",
+          title: "Eight pug gifs that remind me of mitch",
+          topic: "mitch",
+          votes: 0,
+        };
+        expect(response.body.articles[0]).toMatchObject(article);
+      });
+  });
 
-    test("Get: 200 sends an array of topics sorted by date created in descending order", () => {
-        return request(app)
-        .get("/api/articles")
-        .expect(200)
-        .then((response) => {
-            expect(response.body.articles).toBeSortedBy("created_at", {descending: true,})
-         })
-    })
-})
-
+  test("Get: 200 sends an array of topics sorted by date created in descending order", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then((response) => {
+        expect(response.body.articles).toBeSortedBy("created_at", {
+          descending: true,
+        });
+      });
+  });
+});
 
 describe("/api/articles/:article_id", () => {
-    test("Get: 200 sends an object with the correct properties", () => {
-        return request(app)
-        .get("/api/articles/1")
-        .expect(200)
-        .then((response) => {
-            expect(response.body.article).toMatchObject({
-                article_id: 1,
-                title: "Living in the shadow of a great man",
-                topic: "mitch",
-                author: "butter_bridge",
-                body: "I find this existence challenging",
-                created_at: "2020-07-09T20:11:00.000Z",
-                votes: 100,
-                article_img_url:
-                  "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
-              })
-        })
-    })
+  test("Get: 200 sends an object with the correct properties", () => {
+    return request(app)
+      .get("/api/articles/1")
+      .expect(200)
+      .then((response) => {
+        expect(response.body.article).toMatchObject({
+          article_id: 1,
+          title: "Living in the shadow of a great man",
+          topic: "mitch",
+          author: "butter_bridge",
+          body: "I find this existence challenging",
+          created_at: "2020-07-09T20:11:00.000Z",
+          votes: 100,
+          article_img_url:
+            "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+        });
+      });
+  });
 
-    test("Get: 200 sends an object with the correct properties", () => {
-        return request(app)
-        .get("/api/articles/3")
-        .expect(200)
-        .then((response) => {
-            expect(response.body.article).toMatchObject({
-                article_id: 3,
-                title: "Eight pug gifs that remind me of mitch",
-                topic: "mitch",
-                author: "icellusedkars",
-                body: "some gifs",
-                created_at: "2020-11-03T09:12:00.000Z",
-                article_img_url:
-                  "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
-              })
-        })
-    })
+  test("Get: 200 sends an object with the correct properties", () => {
+    return request(app)
+      .get("/api/articles/3")
+      .expect(200)
+      .then((response) => {
+        expect(response.body.article).toMatchObject({
+          article_id: 3,
+          title: "Eight pug gifs that remind me of mitch",
+          topic: "mitch",
+          author: "icellusedkars",
+          body: "some gifs",
+          created_at: "2020-11-03T09:12:00.000Z",
+          article_img_url:
+            "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+        });
+      });
+  });
 
-    test("Get: 404 sends an appropriate status and error message when given a valid but non-existent id", () => {
-        return request(app)
-        .get("/api/articles/888")
-        .expect(404)
-        .then((response) => {
-            expect(response.body.msg).toBe("article does not exist")
-        })
-    })
+  test("Get: 404 sends an appropriate status and error message when given a valid but non-existent id", () => {
+    return request(app)
+      .get("/api/articles/888")
+      .expect(404)
+      .then((response) => {
+        expect(response.body.msg).toBe("article does not exist");
+      });
+  });
 
-    test("Get: 400 sends an appropriate status and error message when given an invalid id", () => {
-        return request(app)
-        .get("/api/articles/not-an-article")
-        .expect(400)
-        .then((response) => {
-            expect(response.body.msg).toBe("Bad request")
-        })
-    })
-})
+  test("Get: 400 sends an appropriate status and error message when given an invalid id", () => {
+    return request(app)
+      .get("/api/articles/not-an-article")
+      .expect(400)
+      .then((response) => {
+        expect(response.body.msg).toBe("Bad request");
+      });
+  });
+});
 
 describe("/api/articles/:article_id/comments", () => {
-    test("Get: 200 sends an array of comments belonging to a single article to the client", () => {
-        return request(app)
-        .get("/api/articles/1/comments")
-        .expect(200)
-        .then((response) => {
-            const commentObj = {
-                comment_id: 9,
-                body: 'Superficially charming',
-                article_id: 1,
-                author: 'icellusedkars',
-                votes: 0,
-                created_at: '2020-01-01T03:08:00.000Z'
-              }
-            expect(response.body.comments[response.body.comments.length -1]).toMatchObject(commentObj)
-            expect(response.body.comments.length).toBe(11)
-            response.body.comments.forEach((comment) => {
-                expect(typeof comment.comment_id).toBe("number")
-                expect(typeof comment.votes).toBe("number")
-                expect(typeof comment.created_at).toBe("string")
-                expect(typeof comment.author).toBe("string")
-                expect(typeof comment.body).toBe("string")
-                expect(typeof comment.article_id).toBe("number")
-            })
-        })
-    })
+  test("Get: 200 sends an array of comments belonging to a single article to the client", () => {
+    return request(app)
+      .get("/api/articles/1/comments")
+      .expect(200)
+      .then((response) => {
+        const commentObj = {
+          comment_id: 9,
+          body: "Superficially charming",
+          article_id: 1,
+          author: "icellusedkars",
+          votes: 0,
+          created_at: "2020-01-01T03:08:00.000Z",
+        };
+        expect(
+          response.body.comments[response.body.comments.length - 1]
+        ).toMatchObject(commentObj);
+        expect(response.body.comments.length).toBe(11);
+        response.body.comments.forEach((comment) => {
+          expect(typeof comment.comment_id).toBe("number");
+          expect(typeof comment.votes).toBe("number");
+          expect(typeof comment.created_at).toBe("string");
+          expect(typeof comment.author).toBe("string");
+          expect(typeof comment.body).toBe("string");
+          expect(comment.article_id).toBe(1);
+        });
+      });
+  });
 
-    test("Get: 200 sends an array of comments with recents comments first", () => {
-        return request(app)
-        .get("/api/articles/3/comments")
-        .expect(200)
-        .then((response) => {
-            expect(response.body.comments.length).toBe(2)
-            expect(response.body.comments).toBeSortedBy("created_at", {descending: true,})
-        })
-    })
+  test("Get: 200 sends an array of comments with recents comments first", () => {
+    return request(app)
+      .get("/api/articles/3/comments")
+      .expect(200)
+      .then((response) => {
+        expect(response.body.comments.length).toBe(2);
+        expect(response.body.comments).toBeSortedBy("created_at", {
+          descending: true,
+        });
+      });
+  });
 
-    test("Get: 404 sends an appropriate status error message when given a valid but non-existent id", () => {
-        return request(app)
-        .get("/api/articles/399/comments")
-        .expect(404)
-        .then((response) => {
-            expect(response.body.msg).toBe("not found")
-        })
-    })
+  test("Get: 404 sends an appropriate status error message when given a valid but non-existent id", () => {
+    return request(app)
+      .get("/api/articles/399/comments")
+      .expect(404)
+      .then((response) => {
+        expect(response.body.msg).toBe("not found");
+      });
+  });
 
-    test("Get: 400 sends an appropriate status error message when given an invalid id", () => {
-        return request(app)
-        .get("/api/articles/not-an-id/comments")
-        .expect(400)
-        .then((response) => {
-            expect(response.body.msg).toBe("Bad request")
-        })
-    })
+  test("Get: 400 sends an appropriate status error message when given an invalid id", () => {
+    return request(app)
+      .get("/api/articles/not-an-id/comments")
+      .expect(400)
+      .then((response) => {
+        expect(response.body.msg).toBe("Bad request");
+      });
+  });
 
-    test("Get: 200 sends back an empty array to the client, when article id exist, but there is no comments for the id", () => {
-        return request(app)
-        .get("/api/articles/2/comments")
-        .expect(200)
-        .then((response) => {
-            expect(response.body.comments).toEqual([])
-        })
-    })
-})
+  test("Get: 200 sends back an empty array to the client, when article id exist, but there is no comments for the id", () => {
+    return request(app)
+      .get("/api/articles/2/comments")
+      .expect(200)
+      .then((response) => {
+        expect(response.body.comments).toEqual([]);
+      });
+  });
+});
--- controllers/articles-controllers.js
+++ controllers/articles-controllers.js
@@ -1,37 +1,42 @@
 const { checkExists } = require("../utlis");
-const { selectArticlesById, selectArticle, selectCommentsByArticleId } = require("../models/articles-models");
+const {
+  selectArticlesById,
+  selectArticle,
+  selectCommentsByArticleId,
+} = require("../models/articles-models");
 
 exports.getArticle = (req, res, next) => {
-    selectArticle().then((articles) => {
-        res.status(200).send({articles})
-    })
-}
+  selectArticle().then((articles) => {
+    res.status(200).send({ articles });
+  });
+};
 
 exports.getArticlesById = (req, res, next) => {
-    const {article_id} = req.params;
-    selectArticlesById(article_id)
+  const { article_id } = req.params;
+  selectArticlesById(article_id)
     .then((article) => {
-        res.status(200).send({article});
-    }).catch((err) => {
-        next(err);
-    })
-}
-
-exports.getCommentsByArticleId = (req, res, next) => {
-    const {article_id} = req.params
-
-    const articlePromises = [selectCommentsByArticleId(article_id)]
-
-    if (article_id) {
-        articlePromises.push(checkExists("articles", "article_id", article_id))
-    }
-
-    Promise.all(articlePromises)
-    .then((resolvedPromises) => {
-        const comments = resolvedPromises[0]
-        res.status(200).send( { comments })
+      res.status(200).send({ article });
     })
     .catch((err) => {
-        next(err)
+      next(err);
+    });
+};
+
+exports.getCommentsByArticleId = (req, res, next) => {
+  const { article_id } = req.params;
+
+  const articlePromises = [selectCommentsByArticleId(article_id)];
+
+  if (article_id) {
+    articlePromises.push(checkExists("articles", "article_id", article_id));
+  }
+
+  Promise.all(articlePromises)
+    .then((resolvedPromises) => {
+      const comments = resolvedPromises[0];
+      res.status(200).send({ comments });
     })
-}
+    .catch((err) => {
+      next(err);
+    });
+};
