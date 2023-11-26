ce49c9bdb7d70cb2e2c2ae1c41a1c137ba648d04
Sakhee89 <kung_fu_fighting_monkey@hotmail.com>
2023-11-26 23:28:44 +0000

    add pagination for the get articles endpoint

--- errors.js
+++ errors.js
@@ -1,5 +1,10 @@
 exports.handlePsqlErrors = (err, req, res, next) => {
-  if (err.code === "22P02" || err.code === "23502") {
+  if (
+    err.code === "22P02" ||
+    err.code === "23502" ||
+    err.code === "2201W" ||
+    err.code === "2201X"
+  ) {
     res.status(400).send({ msg: `Bad request` });
   } else if (err.code === "23503") {
     res.status(404).send({ msg: "Not found" });
